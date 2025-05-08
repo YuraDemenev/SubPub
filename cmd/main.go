d8598efbd28f0989a20b1e7ae9ad1a8bc1b381b7
YuraDemenev <132361342+YuraDemenev@users.noreply.github.com>
2025-05-08 16:14:54 +0500

    Generate proto file, created config

--- cmd/main.go
+++ cmd/main.go
@@ -1,45 +1,18 @@
 package main
 
 import (
-	"context"
 	"fmt"
-	"subpun/subpub"
-	"time"
+	"log"
+	"subpun/internal/config"
 )
 
 func main() {
-	sp := subpub.NewSubPub()
-
-	// Subscriber 1: Fast
-	_, err := sp.Subscribe("user_registered", func(msg interface{}) {
-		fmt.Println("Subscriber 1: Received:", msg)
-	})
+	cfg, err := config.LoadConfig()
 	if err != nil {
-		fmt.Println("Error:", err)
+		log.Fatalf("Failed to load config: %v", err)
 	}
 
-	// Subscriber 2: Slow
-	_, err = sp.Subscribe("user_registered", func(msg interface{}) {
-		time.Sleep(1 * time.Second)
-		fmt.Println("Subscriber 2: Received:", msg)
-	})
-	if err != nil {
-		fmt.Println("Error:", err)
-	}
-
-	// Publish messages
-	sp.Publish("user_registered", "user123")
-	sp.Publish("user_registered", "user456")
-
-	// Allow some time for processing
-	time.Sleep(2 * time.Second)
-
-	// Close with timeout
-	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Millisecond)
-	defer cancel()
-	if err := sp.Close(ctx); err != nil {
-		fmt.Println("Close error:", err)
-	}
-
-	fmt.Println("Program finished")
+	fmt.Printf("gRPC Port: %d\n", cfg.GRPC.Port)
+	fmt.Printf("Logging Level: %s\n", cfg.Logging.Level)
+	fmt.Printf("Message Channel Size: %d\n", cfg.SubPub.MessageChannelSize)
 }
---
+++ internal/config/config.go
@@ -0,0 +1,60 @@
+package config
+
+import (
+	"fmt"
+
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	GRPC struct {
+		Port int `mapstructure:"port"`
+	} `mapstructure:"grpc"`
+	Logging struct {
+		Level string `mapstructure:"level"`
+	} `mapstructure:"logging"`
+	SubPub struct {
+		MessageChannelSize int `mapstructure:"message_channel_size"`
+	} `mapstructure:"subpub"`
+}
+
+func LoadConfig() (*Config, error) {
+	v := viper.New()
+
+	v.SetConfigName("config")
+	v.SetConfigType("yaml")
+	v.AddConfigPath("../config")
+
+	//Read config
+	if err := v.ReadInConfig(); err != nil {
+		return nil, fmt.Errorf("Failed to read config file err:%s", err.Error())
+	}
+
+	//create config struct
+	var cfg Config
+
+	if err := v.Unmarshal(&cfg); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal config: %s", err.Error())
+	}
+
+	//Check validation
+	if err := cfg.validate(); err != nil {
+		return nil, fmt.Errorf("invalid config: %s", err.Error())
+	}
+	return &cfg, nil
+}
+
+func (c *Config) validate() error {
+	if c.GRPC.Port <= 0 || c.GRPC.Port > 65535 {
+		return fmt.Errorf("invalid grpc port: %d", c.GRPC.Port)
+	}
+	if c.SubPub.MessageChannelSize <= 0 {
+		return fmt.Errorf("invalid message channel size %d", c.SubPub.MessageChannelSize)
+	}
+	//Check log level
+	if _, err := logrus.ParseLevel(c.Logging.Level); err != nil {
+		return fmt.Errorf("invalid logging level: %s", c.Logging.Level)
+	}
+	return nil
+}
